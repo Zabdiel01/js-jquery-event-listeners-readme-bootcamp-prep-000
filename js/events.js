0341725120f510835a42894b717c0444d01c58f7
Zabdiel Ramos <zabdiel.ramos01@gmail.com>
2018-03-14 06:37:12 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,7 +11,14 @@ function frameIt() {
 }
 function pressIt() {
   $('input').on('keydown', function(e) {
-    if (e.which == )
+    if (e.which == 71) {
+      alert('G was pressed!')
+    }
+  })
+}
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
   })
 }
 
